1f411302a4970781f5e876b11ff67d78c59471af
COTTE <Tristan.COTTE@sgs.com>
2023-05-17 16:31:28 +0200

    Finish function to transform Sly rectangles to Labelme rectangles on dataset

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 import json
+import os
+from tqdm import tqdm
 
-ann_path = "data/1-18564-TA 98-5000-A-S9 _28_04_2023_11_44_45.jpg.json"
-img_path = "data/1-18564-TA 98-5000-A-S9 _28_04_2023_11_44_45.jpg"
+# ann_path = "data/1-18564-TA 98-5000-A-S9 _28_04_2023_11_44_45.jpg.json"
+# img_path = "data/1-18564-TA 98-5000-A-S9 _28_04_2023_11_44_45.jpg"
+#
+# img_name = "1-18564-TA 98-5000-A-S9 _28_04_2023_11_44_45.jpg"
 
-img_name = "1-18564-TA 98-5000-A-S9 _28_04_2023_11_44_45.jpg"
 
-# Opening JSON file
-f = open(ann_path)
-data = json.load(f)
+OUTPUT_FOLDER = r"C:\Users\tristan_cotte\PycharmProjects\Supervisely2Labelme\Output"
+INPUT_FOLDER = r"C:\Users\tristan_cotte\PycharmProjects\Supervisely2Labelme\sly_dataset\ann"
+extension = ".jpg"
 
-objects = data["objects"]
+for json_name in tqdm(os.listdir(INPUT_FOLDER)):
+    img_name = json_name.split(".")[0]
+    ann_path = os.path.join(INPUT_FOLDER, json_name)
 
-labelme_shapes = []
+    # Opening JSON file
+    f = open(ann_path)
+    data = json.load(f)
 
-for obj in objects:
-    d = {
-        'label': 'germ',
-        'points': obj["points"]["exterior"],
-        'group_id': None,
-        'shape_type': 'rectangle',
-        'flags': {}
+    objects = data["objects"]
+
+    labelme_shapes = []
+
+    for obj in objects:
+        labelme_shapes.append({
+            'label': 'germ',
+            'points': obj["points"]["exterior"],
+            'group_id': None,
+            'shape_type': 'rectangle',
+            'flags': {}
+        })
+
+    annotation = {
+        "version": "4.2.5",
+        "flags": {},
+        "shapes": labelme_shapes,
+        "imagePath": img_name + extension,
+        "imageData": None,
+        "imageHeight": data["size"]["height"],
+        "imageWidth": data["size"]["width"]
     }
-    labelme_shapes.append(d)
 
-annotation = {
-    "version": "4.2.5",
-    "flags": {},
-    "shapes": labelme_shapes,
-    "imagePath": img_name,
-    "imageData": None,
-    "imageHeight": data["size"]["height"],
-    "imageWidth": data["size"]["width"]
-}
-
-
-with open(img_name+'.json', 'w') as outfile:
-    json.dump(annotation, outfile)
+    with open(os.path.join(OUTPUT_FOLDER, img_name + '.json'), 'w') as outfile:
+        json.dump(annotation, outfile)
